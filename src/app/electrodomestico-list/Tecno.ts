698079d180201824c241090d353d56fdb8a321c6
Cecilia Carlon <ceciliacarlon2@gmail.com>
2020-11-24 02:31:26 -0300

    Se agrego una interface, varias directivas y pipes

---
+++ src/app/electrodomestico-list/Tecno.ts
@@ -0,0 +1,8 @@
+export interface Tecno{
+    titulo: string;
+    descripcion: string;
+    precio: number;
+    stock: number;
+    oferta: boolean;
+    imagen: string;
+}
--- src/app/electrodomestico-list/electrodomestico-list.component.ts
+++ src/app/electrodomestico-list/electrodomestico-list.component.ts
@@ -1,35 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Tecno } from './tecno';
 
 @Component({
   selector: 'app-electrodomestico-list',
   templateUrl: './electrodomestico-list.component.html',
   styleUrls: ['./electrodomestico-list.component.scss']
 })
-export class ElectrodomesticoListComponent implements OnInit {
+export class ElectrodomesticoListComponent {
 
-  tecno1={
-    "titulo": "Samsung Galaxy S7",
-    "descripcion": "Celular alta gama",
-    "precio": 150000,
-    "stock": 10,
-    "imagen": "assets/img/samsungs7.jpg",
+  productos: Tecno[]=[
+  {
+    titulo: "Samsung Galaxy S7",
+    descripcion: "Celular alta gama",
+    precio: 150000,
+    stock: 10,
+    oferta: true,
+    imagen: "assets/img/samsungs7.jpg",
+  },
+  {
+    titulo: "Iphone X",
+    descripcion: "A 10 años de el primer Iphone",
+    precio: 500000,
+    stock: 0,
+    oferta: false,
+    imagen: "assets/img/iphonex.jpg",
+  },
+  {
+    titulo: "Samsung Galaxy Note10",
+    descripcion: "Celular alta gama para diseño",
+    precio: 100000,
+    stock: 24,
+    oferta: false,
+    imagen: "assets/img/note10.png",
   }
-
-  tecno2={
-    "titulo": "Iphone X",
-    "descripcion": "A 10 años de el primer Iphone",
-    "precio": 500000,
-    "stock": 20,
-    "imagen": "assets/img/iphonex.jpg",
-  }
-
-  tecno3={
-    "titulo": "Samsung Galaxy Note10",
-    "descripcion": "Celular alta gama para diseño",
-    "precio": 100000,
-    "stock": 24,
-    "imagen": "assets/img/note10.png",
-  }
+  ];
 
   constructor() { }
 
